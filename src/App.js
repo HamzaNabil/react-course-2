cdd5149f388af562ffbf7f57935281c4dd14e024
HamzaNabil <hamzanabil913@gmail.com>
2022-01-22 23:48:59 +0200

    start project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Home from "./Home";
+import "./App.css";
+
+// Class Based Component
+
+// Function Based Component
+
+function App() {
+  return (
+    <div className="app">
+      <i className="fa fa-phone"></i>
+      <h2 className="red">this is H2</h2>
+      <p>This is P</p>
+      <span> this is span</span>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
